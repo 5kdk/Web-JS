0d96851ce4cac581e7e81c2e6458e400161b1e55
5kdk <tylerk1121@gmail.com>
2022-11-22 02:48:40 +0900

    상품 데이터 연결

--- js_basic_market/main.js
+++ js_basic_market/main.js
@@ -1,31 +1,15 @@
-import { getProductList } from "./module/productList.js";
 import { getProductSection } from "./module/productSection.js";
 
-const productSection = getProductSection('인기 상품', [
-  {
-    id: 3,
-    imgSrc: "/js_basic_market/public/assets/바나나.jpg",
-    name: "바나나 1.3kg",
-    discountPercent: 10,
-    price: 11700,
-    originalPrice: 13000,
-  },
-  {
-    id: 4,
-    imgSrc: "/js_basic_market/public/assets/원두.jpg",
-    name: "원두 1kg",
-    discountPercent: 10,
-    price: 9000,
-    originalPrice: 1000,
-  },
-  {
-    id: 5,
-    imgSrc: "/js_basic_market/public/assets/아보카도.jpg",
-    name: "아보카도",
-    discountPercent: 20,
-    price: 16000,
-    originalPrice: 20000,
-  },
-]);
+try {
+  const response = await fetch("./public/mock/sectionListData.json");
+  const data = await response.json();
+  const sectionInfoList = data.sectionInfoList;
 
-document.body.appendChild(productSection);
+  sectionInfoList.forEach((sectionInfo) => {
+    const { sectionTitle, productList } = sectionInfo;
+    const productSectionDOM = getProductSection(sectionTitle, productList);
+    document.body.appendChild(productSectionDOM);
+  });
+} catch (error) {
+  console.log(error);
+}
